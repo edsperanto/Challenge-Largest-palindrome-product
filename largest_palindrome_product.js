383dc53c735c58beac46959931103fccbb8468b5
Edward Gao <egao@sent.com>
2016-11-26 10:01:33 -1000

    added a bunch of comments

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -6,6 +6,10 @@
  *                   the palindromeNumber and the palindromeNumber itself.
  */
 module.exports = function(digits){
+
+  // do your work here
+
+  // variables
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
@@ -13,8 +17,6 @@ module.exports = function(digits){
   var product = 0;
   var temp0 = 0;
   var temp1 = 0;
-
-  // do your work here
 
   // calculate max number for each factor
   for(var i = 0; i < digits; i++) {
@@ -24,10 +26,12 @@ module.exports = function(digits){
   // loop through numbers
   for(temp0 = 10 * (digits-1); temp0 <= maxNum; temp0++) {
     for(temp1 = 10 * (digits-1); temp1 <= maxNum; temp1++) {
-      product = temp0 * temp1;
+      product = temp0 * temp1; // get product
+      // if product is symmetric, confirm new answer
       if(checkSymmetry(product)) {
         factor_0 = temp0;
         factor_1 = temp1;
+        // only update palindrome number when product is a bigger number
         if(product > palindromeNumber) {
           palindromeNumber = product;
         }
@@ -37,27 +41,32 @@ module.exports = function(digits){
 
   // check symmetry
   function checkSymmetry(num) {
-    var fullStr = num.toString();
-    var fullLength = fullStr.length;
-    var halfLength;
-    var match = true;
-    var i;
-    var j;
+    // variables
+    var fullStr = num.toString(); // turn num to string
+    var fullLength = fullStr.length; // get string length
+    var halfLength; // string half length holder
+    var match = true; // default match to true
+    var i; // holder for position when looping from start
+    var j; // holder for position when looping from end
+
+    // get half length depending on if length divisible by 2
     if(fullStr.length % 2 === 0) {
       halfLength = fullStr.length / 2;
     }else{
       halfLength = (fullStr.length - 1) / 2;
     }
-    i = 0;
-    j = (fullLength - 1);
-    for(i; i < halfLength; i++) { // loop from beginnning
 
-      if(fullStr.charAt(i) !== fullStr.charAt(j)) {
-        match = false;
+    i = 0; // beginning start point
+    j = (fullLength - 1); // end start point
+    while(i < halfLength) { // loop through string
+      if(fullStr.charAt(i) !== fullStr.charAt(j)) { // if one digit does not match
+        match = false; // match is false
       }
-      j--;
-      //console.log(fullStr, fullStr.charAt(i), fullStr.charAt(j), match);
+      i++; // loop from start
+      j--; // loop from end
     }
+
+    // return answer
     return match;
   }
 
